c682044e66f6b90d6878feaad3b27f7fa0bac283
CondeSaheki <48620486+CondeSaheki@users.noreply.github>
2025-01-05 22:10:30 -0300

    moved fft

---
+++ StoryBrew.Common/Util/Audio/FftStream.cs
@@ -0,0 +1,54 @@
+﻿using ManagedBass;
+
+namespace StoryBrew.Util.Audio;
+
+internal class FftStream : IDisposable
+{
+    private int stream;
+    private ChannelInfo info;
+
+    private readonly float frequency;
+
+    /// <summary>
+    /// Samples per second
+    /// </summary>
+    public float Frequency => frequency;
+
+    public double Duration { get; }
+
+    public FftStream(string path)
+    {
+        stream = Bass.CreateStream(path, 0, 0, BassFlags.Decode | BassFlags.Prescan);
+        Duration = Bass.ChannelBytes2Seconds(stream, Bass.ChannelGetLength(stream));
+        info = Bass.ChannelGetInfo(stream);
+
+        Bass.ChannelGetAttribute(stream, ChannelAttribute.Frequency, out frequency);
+    }
+
+    public float[] GetFft(double time, bool splitChannels = false)
+    {
+        var position = Bass.ChannelSeconds2Bytes(stream, time);
+        Bass.ChannelSetPosition(stream, position);
+
+        var size = 1024;
+        var flags = DataFlags.FFT2048;
+
+        if (splitChannels)
+        {
+            size *= info.Channels;
+            flags |= DataFlags.FFTIndividual;
+        }
+
+        var data = new float[size];
+
+        if (Bass.ChannelGetData(stream, data, unchecked((int)flags)) == -1) throw new InvalidOperationException($"Failed to get FFT data at {time} seconds.");
+
+        return data;
+    }
+
+    public void Dispose()
+    {
+        Bass.StreamFree(stream);
+        stream = 0;
+    }
+}
