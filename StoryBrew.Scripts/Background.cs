2c8afc8d270b2e492f7c0b96477dab63bff2c14e
tikliz <tik8lizz@gmail.com>
2025-01-14 20:30:44 -0300

    removed commented out code

--- StoryBrew.Scripts/Background.cs
+++ StoryBrew.Scripts/Background.cs
@@ -1,34 +1,4 @@
-﻿// using StoryBrew.Scripting;
-// using StoryBrew.Storyboarding;
-
-// namespace Storybrew.Scripts;
-
-// public class Background : StoryboardObjectGenerator
-// {
-//     [Group("Timing")]
-//     [Configurable] public int StartTime = 0;
-//     [Configurable] public int EndTime = 0;
-
-//     [Group("Sprite")]
-//     [Description("Leave empty to automatically use the map's background.")]
-//     [Configurable] public string SpritePath = "";
-//     [Configurable] public double Opacity = 0.2;
-
-//     public override void Generate()
-//     {
-//         if (SpritePath == "") SpritePath = Beatmap.BackgroundPath ?? string.Empty;
-//         if (StartTime == EndTime) EndTime = (int)(Beatmap.HitObjects.LastOrDefault()?.EndTime ?? AudioDuration);
-
-//         var bitmap = GetMapsetBitmap(SpritePath);
-//         var bg = GetLayer("").CreateSprite(SpritePath, OsbOrigin.Centre);
-//         bg.Scale(StartTime, 480.0f / bitmap.Height);
-//         bg.Fade(StartTime - 500, StartTime, 0, Opacity);
-//         bg.Fade(EndTime, EndTime + 500, Opacity, 0);
-//     }
-// }
-
-
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
 using OpenTK.Mathematics;
 using SkiaSharp;
 using StoryBrew.Mapset;
@@ -70,6 +40,8 @@ public class Background : Script
 
         if (RngSeed != null) {
             random = new Random((int)RngSeed);
+
+
             // TODO: fix this
             bg.Rotate(StartTime, randFloatRange(0, MathHelper.DegreesToRadians(360)));
         }
