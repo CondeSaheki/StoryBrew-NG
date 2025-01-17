3a10b523bc38cf2ad53ba33bd9412b72fe99299d
CondeSaheki <48620486+CondeSaheki@users.noreply.github>
2025-01-17 11:18:23 -0300

    add: new storyboard element classes initial implementation

---
+++ StoryBrew/Storyboarding/Loop.cs
@@ -0,0 +1,13 @@
+
+using StoryBrew.Storyboarding;
+using StoryBrew.Project.Files;
+
+public class Loop : IStoryboardElement
+{
+    public double StartTime => throw new NotImplementedException();
+
+    public void WriteOsb(TextWriter writer, ExportSettings exportSettings, Layer layer, StoryboardTransform? transform)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ StoryBrew/Storyboarding/Trigger.cs
@@ -0,0 +1,13 @@
+
+using StoryBrew.Storyboarding;
+using StoryBrew.Project.Files;
+
+public class Trigger : IStoryboardElement
+{
+    public double StartTime => throw new NotImplementedException();
+
+    public void WriteOsb(TextWriter writer, ExportSettings exportSettings, Layer layer, StoryboardTransform? transform)
+    {
+        throw new NotImplementedException();
+    }
+}
