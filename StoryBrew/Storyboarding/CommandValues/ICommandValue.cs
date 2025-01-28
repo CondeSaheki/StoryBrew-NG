d80c324a4d041010c2aa54dd790f2e11aa40e20f
CondeSaheki <48620486+CondeSaheki@users.noreply.github>
2025-01-28 14:17:29 -0300

    remove command values

--- StoryBrew/Storyboarding/CommandValues/ICommandValue.cs
+++
@@ -1,9 +0,0 @@
-using StoryBrew.Project.Files;
-
-namespace StoryBrew.Storyboarding.CommandValues;
-
-public interface ICommandValue
-{
-    float DistanceFrom(object obj);
-    string ToOsbString(ExportSettings exportSettings);
-}
